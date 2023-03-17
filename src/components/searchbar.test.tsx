13f5b62f721ae67843256c8a14e23b1937851a32
Valts Silaputnins <valts@velns.org>
2023-03-17 13:40:30 +0200

    feat: cover function calls

--- src/components/searchbar.test.tsx
+++ src/components/searchbar.test.tsx
@@ -1,10 +1,37 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { MemoryStorageProvider } from '../providers/memory-storage-provider';
 import SearchBar from './searchbar';
 
 describe('Searchbar component', () => {
   it('should render without crash', async () => {
+    var memory = new MemoryStorageProvider();
+    const qcFunc = jest.fn();
+    const scFunc = jest.fn();
+    const pattern = 'sweetsearch';
+
+    act(() => {
+      render(
+        <SearchBar
+          id="sb"
+          localstore={memory}
+          testId="sb-test"
+          triggerOnLoad={true}
+          onQueryChange={qcFunc}
+          onSearch={scFunc}
+          title={pattern}
+        />
+      );
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(pattern)).toBeInTheDocument();
+    });
+
+    expect(scFunc).toBeCalled();
+  });
+
+  it('should Trigger querychange', async () => {
     var memory = new MemoryStorageProvider();
     const qcFunc = jest.fn();
     const scFunc = jest.fn();
@@ -28,7 +55,13 @@ describe('Searchbar component', () => {
       expect(screen.getByText(pattern)).toBeInTheDocument();
     });
 
+    const searchBar = screen.getByTestId('sb-test');
+    expect(searchBar).toBeInTheDocument();
+
+    act(() => fireEvent.change(searchBar, { target: { value: '12345' } }));
+
     expect(qcFunc).toBeCalled();
-    expect(scFunc).toBeCalled();
   });
+
+
 });
