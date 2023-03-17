341f9b6ae5d2c7f6b281b52e11086eef16c7ae65
Valts Silaputnins <valts@velns.org>
2023-03-17 21:47:52 +0200

    feat: add react logo to about page

--- src/components/about-page.tsx
+++ src/components/about-page.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import reactLogo from '../assets/react.svg';
+import './about-page.css';
 
 class AboutPage extends React.Component {
   render() {
-    return <> Fantastic about page</>;
+    return (
+      <>
+        <p>Fantastic about page of module01 react project</p>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </>
+    );
   }
 }
 
