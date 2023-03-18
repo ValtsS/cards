8f257a05f158a9e119cb2bcc1eff55bc1127ea9e
Valts Silaputnins <valts@velns.org>
2023-03-19 00:03:27 +0200

    feat: adjust text in about

--- src/components/about-page.tsx
+++ src/components/about-page.tsx
@@ -8,6 +8,7 @@ class AboutPage extends React.Component {
       <>
         <p>Fantastic about page of module01 react project</p>
         <img src={reactLogo} className="logo react" alt="React logo" />
+        <p>Too bad I am not a designer at all!</p>
       </>
     );
   }
