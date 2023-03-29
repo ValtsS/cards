dad363bcbd9fb82d390f6f053bc8b9171581181b
Valts Silaputnins <valts@velns.org>
2023-03-29 21:02:52 +0300

    refactor: first custom hook!

---
+++ src/components/searchbar/searchbar-use-localstore.tsx
@@ -0,0 +1,62 @@
+import { useAppContext } from '@/providers';
+import { useRef, useState, useEffect } from 'react';
+
+export interface LocalSearchState {
+  query: string;
+  contextReady: boolean;
+}
+
+export interface UseLocalStoreResult {
+  state: LocalSearchState;
+  handleChange: (filter: string | undefined, search: boolean) => string;
+}
+
+export const useLocalStore = (key: string): UseLocalStoreResult => {
+  const { localStore } = useAppContext();
+  const initialState: LocalSearchState = {
+    contextReady: false,
+    query: '',
+  };
+
+  const lastQueryRef = useRef<string>('');
+
+  const [state, setState] = useState(initialState);
+
+  const getFromStore = (): string => {
+    return localStore.getItem(key) ?? '';
+  };
+
+  const saveToStore = (val: string) => localStore.setItem(key, val);
+
+  useEffect(() => {
+    const loaded = getFromStore();
+    if (loaded) {
+      lastQueryRef.current = loaded;
+      setState({
+        contextReady: true,
+        query: loaded,
+      });
+    }
+    return () => {
+      if (lastQueryRef.current) saveToStore(lastQueryRef.current);
+    };
+  }, [localStore]);
+
+  const handleChange = (filter: string | undefined, search: boolean): string => {
+    const last = state.query;
+    const apply = filter === undefined ? last : filter;
+
+    if (apply != last) {
+      lastQueryRef.current = apply;
+      setState({
+        ...state,
+        query: apply,
+      });
+    }
+
+    if (search) saveToStore(apply);
+    return apply;
+  };
+
+  return { state, handleChange };
+};
--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -1,5 +1,5 @@
-import { useAppContext } from '@/providers/app-context-provider';
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import React, { ReactElement } from 'react';
+import { useLocalStore } from './searchbar-use-localstore';
 import './searchbar.css';
 
 interface SearchProps {
@@ -9,71 +9,16 @@ interface SearchProps {
   title?: string;
 }
 
-interface LocalSearchState {
-  query: string;
-  contextReady: boolean;
-}
-
 export const SearchBar = (props: SearchProps): ReactElement => {
-  const { localStore } = useAppContext();
-
-  const getKey = (): string => {
-    return `searchbar_${props.id}_lastquery`;
-  };
-
-  const initialState: LocalSearchState = {
-    contextReady: false,
-    query: '',
-  };
-
-  const lastQueryRef = useRef<string>('');
-
-  const [state, setState] = useState(initialState);
-
-  const getFromStore = (): string => {
-    return localStore.getItem(getKey()) ?? '';
-  };
-
-  const saveToStore = (val: string) => localStore.setItem(getKey(), val);
-
-  useEffect(() => {
-    const loaded = getFromStore();
-    if (loaded) {
-      lastQueryRef.current = loaded;
-      setState({
-        contextReady: true,
-        query: loaded,
-      });
-    }
-    return () => {
-      if (lastQueryRef.current) saveToStore(lastQueryRef.current);
-    };
-  }, [localStore]);
-
-  const handleChange = (filter: string | undefined, search: boolean) => {
-    const last = state.query;
-    const apply = filter === undefined ? last : filter;
-
-    if (apply != last) {
-      lastQueryRef.current = apply;
-      setState({
-        ...state,
-        query: apply,
-      });
-    }
-
-    if (search) {
-      saveToStore(apply);
-      props.onQueryChange(apply);
-    }
-  };
+  const { state, handleChange } = useLocalStore(`searchbar_${props.id}_lastquery`);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     handleChange(event.target.value, false);
   };
 
   const handleSearch = () => {
-    handleChange(undefined, true);
+    const query = handleChange(undefined, true);
+    props.onQueryChange(query);
   };
 
   const handleKeyPress = (event: React.KeyboardEvent): void => {
