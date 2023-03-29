af643499f10a89f5af9e06cd8d3a5556261e264d
Valts Silaputnins <valts@velns.org>
2023-03-29 18:05:24 +0300

    refactor: conversion into FC

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,34 +1,33 @@
 import { Rating } from '@/components/rating';
 import { CardData } from '@/providers';
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './card.css';
 
 export interface CardProps {
   card: CardData;
 }
 
-export class Card extends React.Component<CardProps> {
-  render() {
-    const imgclass =
-      'bigpic' +
-      (this.props.card.flipimg && this.props.card.flipimg.valueOf() ? ' flip' : '') +
-      (this.props.card.grayscale && this.props.card.grayscale.valueOf() ? ' grayscale' : '');
-    return (
-      <div className="card">
-        <div className="inner">
-          <div className="title">{this.props.card.title}</div>
-          <img src={this.props.card.imageUrl} className={imgclass}></img>
+export const Card = (props: CardProps): ReactElement => {
+  const imgclass =
+    'bigpic' +
+    (props.card.flipimg && props.card.flipimg.valueOf() ? ' flip' : '') +
+    (props.card.grayscale && props.card.grayscale.valueOf() ? ' grayscale' : '');
 
-          <div className="price">
-            <span>{this.props.card.price}</span>
-            <Rating count={this.props.card.rating ?? 0} />
-          </div>
+  return (
+    <div className="card">
+      <div className="inner">
+        <div className="title">{props.card.title}</div>
+        <img src={props.card.imageUrl} className={imgclass}></img>
 
-          <p className="smalltitle">{this.props.card.text}</p>
-          <img src={this.props.card.minipic} className="minipic"></img>
-          <p className="date">{this.props.card.addedat?.toDateString()}</p>
+        <div className="price">
+          <span>{props.card.price}</span>
+          <Rating count={props.card.rating ?? 0} />
         </div>
+
+        <p className="smalltitle">{props.card.text}</p>
+        <img src={props.card.minipic} className="minipic"></img>
+        <p className="date">{props.card.addedat?.toDateString()}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
