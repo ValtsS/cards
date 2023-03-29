8cc642517801807ea3145ab9016c4c99a9af3147
Valts Silaputnins <valts@velns.org>
2023-03-29 23:21:40 +0300

    feat: improve card tests

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -1,7 +1,8 @@
+import { CardData } from '@/providers';
 import { cardTestData } from '@/providers/card/card-test-data';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { Card } from './card';
+import { Card, IMGCLS_FLIP, IMGCLS_GRAY } from './card';
 
 describe('Card component', () => {
   it.each(cardTestData)('should render the card data correctly for $title', (card) => {
@@ -9,5 +10,44 @@ describe('Card component', () => {
     expect(screen.getByText(card.title ?? '')).toBeInTheDocument();
     expect(screen.getByText(card.text ?? '')).toBeInTheDocument();
     expect(screen.getByText(card.price ?? '')).toBeInTheDocument();
+  });
+
+  const flipsGrays: CardData[] = [
+    {
+      uuid: 'AA',
+      flipimg: false,
+    },
+    {
+      uuid: 'BB',
+      grayscale: false,
+    },
+    {
+      uuid: 'CC',
+      flipimg: true,
+    },
+    {
+      uuid: 'DD',
+      grayscale: true,
+    },
+    {
+      uuid: 'EE',
+    },
+    {
+      uuid: 'EE',
+    },
+  ];
+
+  it.each(flipsGrays)('should render the cards flips/grays correctly $uuid', (card) => {
+    render(<Card card={card} />);
+    expect(screen.getByText('☆☆☆☆☆')).toBeInTheDocument();
+    const image = screen.getByRole('img', { name: /full picture/i });
+
+    expect(image).toBeInTheDocument();
+
+    if (card.grayscale) expect(image).toHaveClass(IMGCLS_GRAY);
+    else expect(image).not.toHaveClass(IMGCLS_GRAY);
+
+    if (card.flipimg) expect(image).toHaveClass(IMGCLS_FLIP);
+    else expect(image).not.toHaveClass(IMGCLS_FLIP);
   });
 });
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -7,17 +7,21 @@ export interface CardProps {
   card: CardData;
 }
 
+export const IMGCLS_FLIP = 'flip';
+export const IMGCLS_GRAY = 'grayscale';
+export const IMGCLS_BIGPIC = 'bigpic';
+
 export const Card = (props: CardProps): ReactElement => {
   const imgclass =
-    'bigpic' +
-    (props.card.flipimg && props.card.flipimg.valueOf() ? ' flip' : '') +
-    (props.card.grayscale && props.card.grayscale.valueOf() ? ' grayscale' : '');
+    IMGCLS_BIGPIC +
+    (props.card.flipimg && props.card.flipimg.valueOf() ? ' ' + IMGCLS_FLIP : '') +
+    (props.card.grayscale && props.card.grayscale.valueOf() ? ' ' + IMGCLS_GRAY : '');
 
   return (
     <div className="card">
       <div className="inner">
         <div className="title">{props.card.title}</div>
-        <img src={props.card.imageUrl} className={imgclass}></img>
+        <img src={props.card.imageUrl} className={imgclass} alt="full picture"></img>
 
         <div className="price">
           <span>{props.card.price}</span>
