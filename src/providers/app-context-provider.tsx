133ed6b30079fe808eb044cd02e8595d183901e2
Valts Silaputnins <valts@velns.org>
2023-04-04 20:01:34 +0300

    feat: remove old provider

--- src/providers/app-context-provider.tsx
+++ src/providers/app-context-provider.tsx
@@ -4,34 +4,29 @@ import { LocalStorage } from './storage';
 
 type AppContextValue = {
   localStore: LocalStorage;
-  cardProvider: CardProviderStore;
   formCardProvider: CardProviderStore;
 };
 
 export const AppContext = React.createContext<AppContextValue>({
   localStore: new LocalStorage(),
-  cardProvider: new CardProviderStore(),
   formCardProvider: new CardProviderStore(),
 });
 
 type AppContextProviderProps = {
   children: React.ReactNode;
   localStoreProvider: LocalStorage;
-  cardProvider: CardProviderStore;
   formCardProvider: CardProviderStore;
 };
 
 export const AppContextProvider = ({
   children,
   localStoreProvider,
-  cardProvider,
   formCardProvider: cardProvider2,
 }: AppContextProviderProps) => {
   return (
     <AppContext.Provider
       value={{
         localStore: localStoreProvider,
-        cardProvider: cardProvider,
         formCardProvider: cardProvider2,
       }}
     >
