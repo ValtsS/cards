e0fe0ba283ab50310647c927e916c4b4cffb539d
Valts Silaputnins <valts@velns.org>
2023-04-05 01:57:52 +0300

    fix: disable fragments, added prettifer

--- codegen.ts
+++ codegen.ts
@@ -7,6 +7,11 @@ const config: CodegenConfig = {
   generates: {
     './src/__generated__/': {
       preset: 'client',
+      presetConfig: {
+        fragmentMasking: false,
+        strictScalars: true,
+        defaultScalarType: false,
+      },
     },
   },
   hooks: { afterAllFileWrite: ['prettier --write'] },
