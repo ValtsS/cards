e16e5d5dcb5ad881d5767f1de80ad4fec40fc54b
Valts Silaputnins <valts@velns.org>
2023-04-07 00:26:57 +0300

    feat: testing for updated card

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -1,6 +1,6 @@
 import { CardData } from '@/providers';
 import { cardTestData } from '@/providers/card/card-test-data';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Card, IMGCLS_FLIP, IMGCLS_GRAY } from './card';
 
@@ -11,6 +11,24 @@ describe('Card component', () => {
     expect(screen.getByText(card.text ?? '')).toBeInTheDocument();
     expect(screen.getByText(card.price ?? '')).toBeInTheDocument();
   });
+
+  it.each(cardTestData)('should test click', (card) => {
+    const fn = jest.fn();
+
+    render(<Card card={card} clicked={fn} />);
+    expect(screen.getByText(card.title ?? '')).toBeInTheDocument();
+    expect(screen.getByText(card.text ?? '')).toBeInTheDocument();
+    expect(screen.getByText(card.price ?? '')).toBeInTheDocument();
+
+    const image = screen.getByRole('img', { name: /full picture/i });
+    expect(image).toBeInTheDocument();
+
+    fireEvent.click(image);
+
+    expect(fn).toBeCalledTimes(1);
+
+  });
+
 
   const flipsGrays: CardData[] = [
     {
@@ -31,18 +49,27 @@ describe('Card component', () => {
     },
     {
       uuid: 'EE',
+      rating: 0
     },
     {
       uuid: 'EE',
+      rating: 3
     },
   ];
 
   it.each(flipsGrays)('should render the cards flips/grays correctly $uuid', (card) => {
     render(<Card card={card} />);
-    expect(screen.queryByText('☆☆☆☆☆')).not.toBeInTheDocument();
-    const image = screen.getByRole('img', { name: /full picture/i });
 
+    const image = screen.getByRole('img', { name: /full picture/i });
     expect(image).toBeInTheDocument();
+
+    if (card.rating)
+    {
+      expect(screen.getByText('★'.repeat(card.rating ?? 0))).toBeInTheDocument();
+      expect(screen.getByText('☆'.repeat(5 - (card.rating ?? 0)))).toBeInTheDocument();
+    } else {
+      expect(screen.queryByText('☆'.repeat(5))).not.toBeInTheDocument();
+    }
 
     if (card.grayscale) expect(image).toHaveClass(IMGCLS_GRAY);
     else expect(image).not.toHaveClass(IMGCLS_GRAY);
