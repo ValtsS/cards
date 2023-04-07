04f2e9762b3b7b49da56962b4777e1d4ca6ee8f4
Valts Silaputnins <valts@velns.org>
2023-04-07 21:47:02 +0300

    feat: add tests for SortBy

--- src/providers/card/card-provider.test.tsx
+++ src/providers/card/card-provider.test.tsx
@@ -1,4 +1,7 @@
 import { CardData, CardProviderStore } from './card-provider';
+import * as Schema from '@/__generated__/graphql';
+import { SortEnumType } from '@/__generated__/graphql';
+import { SortBy } from './api-provider';
 
 describe('Card provider', () => {
   it('should provide', () => {
@@ -17,5 +20,37 @@ describe('Card provider', () => {
     const card = new CardData();
     expect(card.uuid).toBe((CardData.counter - 1).toString());
     expect(new CardData().uuid).not.toBe(card.uuid);
+  });
+});
+
+describe('SortBy', () => {
+  it('should return correct sorting string for Price_ASC', () => {
+    const sortingInput: Schema.CardSortInput[] = [SortBy.Price_ASC];
+    const result = SortBy.getSortingStr(sortingInput);
+    expect(result).toEqual(['↑Price']);
+  });
+
+  it('should return correct sorting string for Price_DESC', () => {
+    const sortingInput: Schema.CardSortInput[] = [SortBy.Price_DESC];
+    const result = SortBy.getSortingStr(sortingInput);
+    expect(result).toEqual(['↓Price']);
+  });
+
+  it('should return empty array for empty sorting input', () => {
+    const sortingInput: Schema.CardSortInput[] = [];
+    const result = SortBy.getSortingStr(sortingInput);
+    expect(result).toEqual([]);
+  });
+
+  it('should return correct sorting string for multiple sorting inputs', () => {
+    const sortingInput: Schema.CardSortInput[] = [SortBy.Price_ASC, SortBy.Price_DESC];
+    const result = SortBy.getSortingStr(sortingInput);
+    expect(result).toEqual(['↑Price', '↓Price']);
+  });
+
+  it('should return empty array for unknown sorting input', () => {
+    const sortingInput: Schema.CardSortInput[] = [];
+    const result = SortBy.getSortingStr(sortingInput);
+    expect(result).toEqual([]);
   });
 });
