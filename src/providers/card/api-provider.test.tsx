1b0e8be66d858ce5ba51d45545b7156ebecad07a
Valts Silaputnins <valts@velns.org>
2023-04-07 16:49:57 +0300

    feat: handle case without provider and with API throwing errors

--- src/providers/card/api-provider.test.tsx
+++ src/providers/card/api-provider.test.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { MockGqlApi } from '../../../__mocks__/mock-gql-api';
 import * as Schema from '../../__generated__/graphql';
 import { AppContextProvider } from '../app-context-provider';
-import { NotificationsProvider } from '../notifications-provider';
+import { NotificationsProvider, useNotifications } from '../notifications-provider';
 import { MemoryStorage } from '../storage';
 import { CardsApiProvider, useCardsApiContext } from './api-provider';
 import { CardData, CardProviderStore } from './card-provider';
@@ -13,6 +13,8 @@ const CardApiTester = () => {
   const { state, loadCards, getSingleCard } = useCardsApiContext();
 
   const [single, setSingle] = useState<CardData | null | undefined>();
+
+  const { state: msg } = useNotifications();
 
   useEffect(() => {
     loadCards('123');
@@ -38,6 +40,8 @@ const CardApiTester = () => {
       {ids.map((e) => (
         <p key={e}>{e}</p>
       ))}
+      <p>{'Message=' + msg.message}</p>
+      <p>{'Error=' + msg.error}</p>
     </>
   );
 };
@@ -79,7 +83,7 @@ describe('API provider tests', () => {
     });
   });
 
-  it('Should not crash', async () => {
+  it('Should return single and multi search', async () => {
     act(() => renderDefault(api));
 
     // wait for the component to re-render
@@ -96,15 +100,49 @@ describe('API provider tests', () => {
       expect(screen.getByText('multiuuid:' + e.uuid)).toBeInTheDocument();
     });
   });
+
+  it('Should handle API errors', async () => {
+    const testErr = 'Simulated API error';
+
+    api.clientMock.query = jest.fn().mockImplementation(() => {
+      throw new Error(testErr);
+    });
+    act(() => renderDefault(api));
+
+    // wait for the component to re-render
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+
+    expect(screen.getByText('Loaded')).toBeInTheDocument();
+    expect(screen.getByText('Length:0')).toBeInTheDocument();
+
+    expect(screen.getByText('singleuuid:undefined')).toBeInTheDocument();
+
+    expect(screen.getByText('Message=' + testErr)).toBeInTheDocument();
+    expect(screen.getByText('Error=true')).toBeInTheDocument();
+  });
+
+  it('Should should trigger errors without provider', async () => {
+    act(() => renderDefault(null));
+
+    // wait for the component to re-render
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+
+    expect(screen.getByText('Message=client is not set')).toBeInTheDocument();
+    expect(screen.getByText('Error=true')).toBeInTheDocument();
+  });
 });
 
-function renderDefault(api: MockGqlApi) {
+function renderDefault(api: MockGqlApi | null) {
   render(
     <NotificationsProvider>
       <AppContextProvider
         localStoreProvider={new MemoryStorage()}
         formCardProvider={new CardProviderStore()}
-        apolloClient={api.clientMock}
+        apolloClient={api?.clientMock || null}
       >
         <CardsApiProvider>
           <CardApiTester />
