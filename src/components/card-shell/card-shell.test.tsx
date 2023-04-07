0140b77826a5aa4390b2d139bd92692bfef56d24
Valts Silaputnins <valts@velns.org>
2023-04-07 03:29:40 +0300

    feat: mock the useModalDialog()

--- src/components/card-shell/card-shell.test.tsx
+++ src/components/card-shell/card-shell.test.tsx
@@ -1,7 +1,20 @@
 import { CardData } from '@/providers';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { CardShell } from './card-shell';
+
+const mockShowDialog = jest.fn(() => 'Mocked showDialog return value');
+
+jest.mock('@/providers', () => {
+  const mockuseModalDialog = jest.fn(() => ({
+    showDialog: mockShowDialog,
+  }));
+
+  return {
+    ...jest.requireActual('@/providers'),
+    useModalDialog: mockuseModalDialog,
+  };
+});
 
 describe('Card Shell component', () => {
   it('should not crash', () => {
@@ -25,5 +38,19 @@ describe('Card Shell component', () => {
   it('renders default message when query is undefined', () => {
     const { getByText } = render(<CardShell data={[]} query={undefined} />);
     expect(getByText('no cards found for your search query:')).toBeInTheDocument();
+  });
+
+  it('should call showDialog!', () => {
+    const test = new CardData();
+    test.title = 'Title';
+    test.price = '$100';
+    test.text = 'Text';
+
+    render(<CardShell data={[test]} />);
+
+    const image = screen.getByRole('img');
+    fireEvent.click(image);
+
+    expect(mockShowDialog).toBeCalled();
   });
 });
