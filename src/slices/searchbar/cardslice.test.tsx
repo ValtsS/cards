1f7d27c75d2a9cc0b9168925bc8f81c018dcd32f
Valts Silaputnins <valts@velns.org>
2023-04-12 20:20:34 +0300

    feat: adjust to const

--- src/slices/searchbar/cardslice.test.tsx
+++ src/slices/searchbar/cardslice.test.tsx
@@ -4,12 +4,13 @@ import { searchBarSlice } from './searchbarSlice';
 describe('searchQuery tests', () => {
   it('should trigger a change', async () => {
     const store = setupStore();
+    const testVal = '134567';
 
-    const action = searchBarSlice.actions.change('134567');
+    const action = searchBarSlice.actions.change(testVal);
     // Act
     store.dispatch(action);
 
     let updatedState = store.getState();
-    expect(updatedState.searchBar.query).toBe('134567');
+    expect(updatedState.searchBar.query).toBe(testVal);
   });
 });
