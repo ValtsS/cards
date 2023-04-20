a926b532afb3847b099c92933b5e08bad00d3446
Valts Silaputnins <valts@velns.org>
2023-04-21 02:06:37 +0300

    refactor: remove unused code

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,4 @@
 export * from './storage';
-export * from './form';
 export * from './card';
 export { AppContext, AppContextProvider, useAppContext } from './app-context-provider';
 export * from './modal-dialog';
