610ace6bbdc6771d5b514940d3e7a6d9714aa68d
Valts Silaputnins <valts@velns.org>
2023-04-20 14:48:37 +0300

    feat: navigation tests

--- cypress/e2e/navigation/navigation.cy.ts
+++ cypress/e2e/navigation/navigation.cy.ts
@@ -10,17 +10,48 @@ describe('Navigation checks', () => {
     getActiveMain();
   });
 
-  it('navigate', function () {
+  it('check header link presence and main page', function () {
     cy.url().should('match', /\/$/);
     const activeLink = getActiveMain();
 
     activeLink.click();
 
     getActiveMain();
+
+    const formLink = cy.get('header a').contains('Form');
+    formLink.should('exist');
+
+    const aboutLink = cy.get('header a').contains('About us');
+    aboutLink.should('exist');
   });
 
-  function getActiveMain() {
+  it('check if navigation to form works', function () {
+    const formLink = cy.get('header a').contains('Form');
+    formLink.should('exist');
+
+    formLink.click();
+
+    const link = getActiveLink();
+    link.should('have.text', 'Form');
+  });
+
+  it('check if navigation to about works', function () {
+    const aboutLink = cy.get('header a').contains('About us');
+    aboutLink.should('exist');
+    aboutLink.click();
+
+    const link = getActiveLink();
+    link.should('have.text', 'About us');
+  });
+
+  function getActiveLink() {
     const activeLink = cy.get('header a.active');
+    activeLink.should('exist');
+    return activeLink;
+  }
+
+  function getActiveMain() {
+    const activeLink = getActiveLink();
     activeLink.should('exist').and('have.text', 'Main');
     return activeLink;
   }
