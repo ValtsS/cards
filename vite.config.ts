1da09562d1c50a05d34cd216254d41e5d928af66
Valts Silaputnins <valts@velns.org>
2023-04-21 20:57:25 +0300

    fix: add back react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
       cypress: true,
       requireEnv: false,
     }),
+    react({ fastRefresh: false }),
   ],
   ssr: {
     noExternal: ['@reduxjs/toolkit'],
