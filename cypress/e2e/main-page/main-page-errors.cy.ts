fb73fcdf9727c98f96af456131ce2d9007cc6257
Valts Silaputnins <valts@velns.org>
2023-04-22 00:38:29 +0300

    feat: modal open with network error

--- cypress/e2e/main-page/main-page-errors.cy.ts
+++ cypress/e2e/main-page/main-page-errors.cy.ts
@@ -6,6 +6,36 @@ describe('Main page', () => {
       req.destroy();
     });
     cy.visit('/');
+  });
+
+  it('it should throw errors on modal', function () {
+    cy.intercept('POST', '**/graphql').as('getCards');
+    cy.visit('/');
+
+    waitForQuery();
+
+    cy.intercept('POST', '**/graphql', (req) => {
+      req.destroy();
+    });
+
+    const firstCard = cy.get('main .card-container > .card').first().should('exist');
+    firstCard.click();
+    waitForQuery();
+
+    cy.get('[data-testid="spinner"]').should('be.visible');
     cy.get('.errorfloater').should('be.visible');
   });
+
+  function waitForQuery() {
+    cy.wait('@getCards').wait(250);
+
+    getApiReady().then((val) => {
+      const trimmedVal = val.toLowerCase().trim();
+      expect(trimmedVal).to.be.oneOf(['succeeded', 'idle']);
+    });
+  }
+
+  function getApiReady() {
+    return cy.get('.api-state-container > :nth-child(1) > span').should('exist').invoke('text');
+  }
 });
